d0328937f91c05e955b9fb05b6d547c50b321b3b
제현 안 <wpgus4677@naver.com>
2022-02-08 15:19:47 +0900

    Update Home 화면 구현

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,8 +1,7 @@
-import { useParams } from "react-router-dom";
+
 
 const Diary =() => {
 
-    const {id} = useParams();
     return (
         <div>
             <h1>여기는 Diary</h1>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,11 +1,6 @@
-import { useSearchParams } from "react-router-dom";
 
 const Edit =() => {
 
-    const [searchParams, setSearchParams] =useSearchParams();
-
-    const id = searchParams.get("id");
-    
     return (
         <div>
             <h1>여기는 Edit</h1>
